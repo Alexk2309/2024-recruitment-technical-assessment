791f1ab77000958c219e9b747e7330b9fe80ea0d
Alex Kim <alexkim5682@gmail.com>
2024-02-24 18:31:12 +1100

    Finished backend tasks

--- backend/py/task.py
+++ backend/py/task.py
@@ -13,21 +13,57 @@ class File:
 Task 1
 """
 def leafFiles(files: list[File]) -> list[str]:
-    return []
+    parentFiles = {}
+    for file in files: parentFiles[file.parent] = file
+    noChildren = []
+    for file in files:
+        result = parentFiles.get(file.id)
+        if not result: noChildren.append(file.name)
+
+    return noChildren
 
 
 """
 Task 2
 """
 def kLargestCategories(files: list[File], k: int) -> list[str]:
-    return []
+    categoryDict = {}
+    for file in files:
+        for category in file.categories:
+            categoryDict[category] = categoryDict.get(category, 0) + 1
+
+    sortedCatergories = sorted(categoryDict, key=lambda k:(categoryDict[k], k[::-1]), reverse=True)
+    return sortedCatergories[:k]
 
 
 """
 Task 3
 """
 def largestFileSize(files: list[File]) -> int:
-    return 0
+
+    # Search for child Id
+    def dfs(fileId):
+        currentFile = filesDict[fileId]
+        currentSize = currentFile.size
+        for child in childrenDict[currentFile.id]:
+            currentSize += dfs(child.id)
+        return currentSize
+
+
+    filesDict = {file.id: file for file in files}
+    childrenDict = {file.id: [] for file in files}
+    for file in files:
+        if file.parent != -1:
+            childrenDict[file.parent].append(file)
+
+    maxSize = 0
+    for file in files:
+        if file.parent == -1:
+            maxSize = max(maxSize, dfs(file.id))
+
+    return maxSize
+
+
 
 
 if __name__ == '__main__':
